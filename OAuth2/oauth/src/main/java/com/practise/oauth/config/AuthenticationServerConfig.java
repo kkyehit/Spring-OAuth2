18954ecb783ccd12461b8511a12033828af83c66
kkeyhit <rp01020113994@gmail.com>
2021-10-09 02:11:17 +0900

    어노테이션 수정 및 토큰 유효성 접근 설정 추가

--- OAuth2/oauth/src/main/java/com/practise/oauth/config/AuthenticationServerConfig.java
+++ OAuth2/oauth/src/main/java/com/practise/oauth/config/AuthenticationServerConfig.java
@@ -2,17 +2,19 @@ package com.practise.oauth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 /**
  * @EnableAuthorizationServer : OAuth 관련 endpoints(/oauth/token, /oauth/authorize 등.)가 생성된다.(AuthorizationEndpoint 및 TokenEndpoint를 활성화) 
  */
 
-@Configurable
+@Configuration
 @EnableAuthorizationServer
 public class AuthenticationServerConfig extends AuthorizationServerConfigurerAdapter {
     
@@ -43,7 +45,7 @@ public class AuthenticationServerConfig extends AuthorizationServerConfigurerAda
         clients.inMemory()
             .withClient(CLINET_ID)
             .secret(CLINET_SECRET)
-            .authorizedGrantTypes("client_credentials", "refresh_token")
+            .authorizedGrantTypes("authorization_code", "password", "refresh_token")
             .scopes("read", "write");
     }
 
@@ -52,5 +54,11 @@ public class AuthenticationServerConfig extends AuthorizationServerConfigurerAda
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.authenticationManager(authenticationManager);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        // 토큰유효성(/token/check_token) 접근을 위해 설정
+        security.checkTokenAccess("permitAll()");
     }
 }
