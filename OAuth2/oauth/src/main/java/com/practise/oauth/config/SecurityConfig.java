14ea0d8d06cdec4e1b56cd3406c857dc4a212d1b
kkeyhit <rp01020113994@gmail.com>
2021-10-09 00:52:19 +0900

    불필요한 코드 삭제

--- OAuth2/oauth/src/main/java/com/practise/oauth/config/SecurityConfig.java
+++ OAuth2/oauth/src/main/java/com/practise/oauth/config/SecurityConfig.java
@@ -1,18 +1,14 @@
 package com.practise.oauth.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
-
-import jdk.jfr.Name;
 
 /**
  * Spring Security 설정 
