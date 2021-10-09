b7e6731133e6a607eba5f73aa8194259b3c09250
kkeyhit <rp01020113994@gmail.com>
2021-10-09 16:43:38 +0900

    RestTemplate 설정 추가

---
+++ OAuth2/oauth/src/main/java/com/practise/oauth/config/RestTemplateConfig.java
@@ -0,0 +1,23 @@
+package com.practise.oauth.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * RestTemplate Bean을 생성하고 관리하기 위한 클래스
+ */
+@Configuration
+public class RestTemplateConfig {
+    
+    /**
+     * 기본 RestTemplate을 생성한다.
+     * @return RestTemplate
+     */
+    @Bean
+    public RestTemplate restTemplate(){
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate;
+    }
+}
