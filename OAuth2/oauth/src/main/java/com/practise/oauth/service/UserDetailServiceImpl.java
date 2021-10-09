5ecdb90a208f20f328213fe3f3e072f272bba99c
kkeyhit <rp01020113994@gmail.com>
2021-10-09 12:56:54 +0900

    사용자 정보를 가져 오는 userDetailsService 구현

--- OAuth2/oauth/src/main/java/com/practise/oauth/service/UserDetailServiceImpl.java
+++ OAuth2/oauth/src/main/java/com/practise/oauth/service/UserDetailServiceImpl.java
@@ -15,6 +15,12 @@ public class UserDetailServiceImpl implements UserDetailsService{
     @Autowired
     UserRepository userRepository;
 
+    /**
+     * userRepository에서 username에 맞는 사용자 정보를 가져와 반환한다.
+     * 사용자가 없으면 UsernameNotFoundException을 발생시킨다.
+     * @param username
+     * @throws UsernameNotFoundException
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username+"은 존재하지 않습니다."));
