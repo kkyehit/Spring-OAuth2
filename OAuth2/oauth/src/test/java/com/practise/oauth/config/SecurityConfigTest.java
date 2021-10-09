6d1aac90d0a62c6a1974ae0d080d600d04d22b5c
kkeyhit <rp01020113994@gmail.com>
2021-10-09 13:04:29 +0900

    Authorization: Basic [credentials] 값을 얻기 위한 테스트 코드 작성

--- OAuth2/oauth/src/test/java/com/practise/oauth/config/SecurityConfigTest.java
+++ OAuth2/oauth/src/test/java/com/practise/oauth/config/SecurityConfigTest.java
@@ -1,5 +1,7 @@
 package com.practise.oauth.config;
 
+import java.util.Base64;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +18,18 @@ public class SecurityConfigTest {
 	@DisplayName("Password Encorder Test")
 	void passwordEncoderTest() {
 
-			String rawPassword = "admin";
+		String rawPassword = "admin";
 
-			System.out.println(passwordEncoder.encode(rawPassword));
+		System.out.println(passwordEncoder.encode(rawPassword));
+	}
+
+	@Test
+	@DisplayName("encoded Credentials Test (base64)")
+	void encodedCredentialsTest() {
+		String clientId = "client_id";
+		String clientSecret = "client_secret";
+		String credentials = clientId+":"+clientSecret;
+
+		System.out.println(Base64.getEncoder().encode(credentials.getBytes()));
 	}
 }
