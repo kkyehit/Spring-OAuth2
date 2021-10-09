31077bc1acd787c006f9e36a6a92a8dd6977d01d
kkeyhit <rp01020113994@gmail.com>
2021-10-09 12:57:21 +0900

    @service 어노테이션 추가

--- OAuth2/oauth/src/main/java/com/practise/oauth/service/UserDetailServiceImpl.java
+++ OAuth2/oauth/src/main/java/com/practise/oauth/service/UserDetailServiceImpl.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 /**
  * 유저 정보를 가져오는 역할
  */
+@Service
 public class UserDetailServiceImpl implements UserDetailsService{
     
     @Autowired
