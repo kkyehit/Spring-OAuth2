469aa0d68d1fe06f6802e01de78a7e5fb6576275
kkeyhit <rp01020113994@gmail.com>
2021-10-09 16:53:37 +0900

    반환 형식 변경 및 주석 작성

--- OAuth2/oauth/src/main/java/com/practise/oauth/controller/OAuthController.java
+++ OAuth2/oauth/src/main/java/com/practise/oauth/controller/OAuthController.java
@@ -1,6 +1,7 @@
 package com.practise.oauth.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.practise.oauth.model.OAuthTokenModel;
 import com.practise.oauth.service.OAuthTokenService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +9,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * OAuth 관련 요청 처리
+ */
 @RestController
 public class OAuthController {
     @Autowired
     OAuthTokenService oAuthTokenService;
 
+    /**
+     * OAuth 정보를 반환한다.
+     * @param code
+     * @return
+     * @throws JsonProcessingException
+     */
     @GetMapping("/oauth2/callback")
-    public String callback(@RequestParam String code) throws JsonProcessingException {
-        return oAuthTokenService.getOAuthTokenModelByCode(code).toString();
+    public OAuthTokenModel callback(@RequestParam String code) throws JsonProcessingException {
+        OAuthTokenModel oAuthTokenModel = oAuthTokenService.getOAuthTokenModelByCode(code);
+        return oAuthTokenModel;
     }
 }
