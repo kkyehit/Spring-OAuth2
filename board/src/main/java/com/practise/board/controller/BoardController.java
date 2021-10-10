e1f565df21659d863bd7d7ec20a3db04b11d164e
kkeyhit <rp01020113994@gmail.com>
2021-10-10 16:46:07 +0900

    파라미터 형식 수정

--- board/src/main/java/com/practise/board/controller/BoardController.java
+++ board/src/main/java/com/practise/board/controller/BoardController.java
@@ -2,8 +2,6 @@ package com.practise.board.controller;
 
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import com.practise.board.model.BoardModel;
 import com.practise.board.service.BoardService;
 
@@ -11,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,7 +35,7 @@ public class BoardController {
      */
     @ApiOperation(value = "게사판 생성", notes = "성공시 생성한 게시판 반환")
     @PostMapping("/")
-    public BoardModel create(BoardModel boardModel){
+    public BoardModel create(@RequestBody BoardModel boardModel){
         return boardService.create(boardModel);
     }
      
@@ -58,7 +58,7 @@ public class BoardController {
      */
     @ApiOperation(value = "게사판 조회", notes = "성공시 id에 해당하는 게시판 반환")
     @GetMapping("/{id}")
-    public BoardModel findById(@PathParam("id") Long id){
+    public BoardModel findById(@PathVariable("id") Long id){
         return boardService.findById(id);
     }
 
@@ -70,7 +70,7 @@ public class BoardController {
      */
     @ApiOperation(value = "게사판 수정", notes = "성공시 게시판 수정")
     @PatchMapping("/")
-    public BoardModel update(BoardModel boardModel){
+    public BoardModel update(@RequestBody BoardModel boardModel){
         return boardService.update(boardModel.getId(), boardModel);
     }
     
@@ -81,8 +81,8 @@ public class BoardController {
      * @param id
      */
     @ApiOperation(value = "게사판 삭제", notes = "성공시 id에 해당하는 게시판 삭제")
-    @DeleteMapping("/")
-    public String delete(Long id){
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") Long id){
         boardService.delete(id);
         return "deleted";
     }
